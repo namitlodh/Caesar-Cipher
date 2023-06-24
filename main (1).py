486ff9024aeae547d13bc99b5fa840f0ba06c372
Namit Lodh <77287454+namitlodh@users.noreply.github.com>
2023-06-24 20:36:28 +0530

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -22,10 +22,6 @@ while should_continue:
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
   
-  #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-  #Try running the program and entering a shift number of 45.
-  #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
-  #Hint: Think about how you can use the modulus (%).
   shift = shift % 26
   caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
